00c1ab033bae88a3c9a250053b51429ec128a905
Michael Wheeler <mwheeler@g2crowd.com>
2019-08-06 15:07:22 -0500

    Remove corejs, let consuming app choose which polyfills to use

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,6 @@
 const presets = [
   [
-    '@babel/env',
-    {
-      targets: {
-        browsers: ['last 2 versions', '> 5%', 'ie 9-11'],
-      },
-      useBuiltIns: 'usage',
-      corejs: '2.6'
-    },
+    '@babel/env'
   ],
 ];
 
