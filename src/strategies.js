4b3094f6e4cd3d9a54e08b79d5583679fcd555fc
Michael Wheeler <mwheeler@g2crowd.com>
2024-03-13 09:39:42 -0500

    feat: Remove JQ lite

--- src/strategies.js
+++ src/strategies.js
@@ -1,15 +1,15 @@
 import initiationStrategies from './initiationStrategies';
 
 export const strategies = initiationStrategies({
-  nextTick(pluginFn, $$) {
+  nextTick(pluginFn, el) {
     return window.setTimeout(() => pluginFn(), 0);
   },
 
-  immediate(pluginFn, $$) {
+  immediate(pluginFn, el) {
     return pluginFn() || {};
   },
 
-  hover(pluginFn, $$) {
-    return $$.one('mouseover', () => pluginFn());
+  hover(pluginFn, el) {
+    return el.addEventListener('mouseover', pluginFn, { once: true });
   }
 });
